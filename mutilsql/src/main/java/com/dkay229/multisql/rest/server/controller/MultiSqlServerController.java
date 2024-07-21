0872d01899ae872b13ae07b3dd6756e94d2394b5
dkay29 <dkay29@gmail.com>
2024-07-21 19:54:48 -0400

    KAN-1 Project setup

---
+++ mutilsql/src/main/java/com/dkay229/multisql/rest/server/controller/MultiSqlServerController.java
@@ -0,0 +1,9 @@
+package com.dkay229.multisql.rest.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MultiSqlServerController {
+}
